2e6254b1712385d1b2eef09b8762988b98dc6bf7
Luke J <luke.jennings1@outlook.com>
2024-12-01 15:11:50 +0000

    Update Day1.hs
    condense

--- Days 1-5/Day1.hs
+++ Days 1-5/Day1.hs
@@ -16,22 +16,13 @@ get_ints (x:xs) i | i == 0 = read (take 5 x) : (get_ints xs i)
 quicksort :: [Int] -> [Int]
 quicksort [] = []
 quicksort (x:[]) = [x]
-quicksort xs = let xs' = tail xs
-                   left = quicksort $ filter (<= (head xs)) xs'
-                   right = quicksort $ filter (> (head xs)) xs'
+quicksort xs = let left = quicksort $ filter (<= (head xs)) (tail xs)
+                   right = quicksort $ filter (> (head xs)) (tail xs)
                 in left ++ [head xs] ++ right
 
 part1 :: [String] -> Int
 part1 input = sum $ map (\(x,y) -> abs (x-y)) (zip (quicksort $ get_ints input 0) (quicksort $ get_ints input 1))
 
-occourances :: Int -> [Int] -> Int
-occourances _ [] = 0
-occourances n (x:xs) | x == n    = 1 + occourances n xs
-                     | otherwise = occourances n xs
-
-similarity_score :: [Int] -> [Int] -> Int
-similarity_score [] _ = 0
-similarity_score (x:xs) ys = (x * (occourances x ys)) + similarity_score xs ys
-
 part2 :: [String] -> Int
-part2 input = similarity_score (get_ints input 0) (get_ints input 1)
+part2 input = let ys = (get_ints input 1) 
+               in sum $ map (\x -> x * (length $ filter (== x) ys)) (get_ints input 0) 
